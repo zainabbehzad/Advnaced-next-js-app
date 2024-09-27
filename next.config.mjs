b11bf7d0cf5dfb738302fce33577df22a17755ea
zainabbehzad <168668702+zainabbehzad@users.noreply.github.com>
2024-09-27 11:31:20 +0430

    Setup the next app and created the Files and folders for advanced next app

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['openweathermap.org'], // Allow images from OpenWeather
+    },
+  };
+  
+  export default nextConfig;
